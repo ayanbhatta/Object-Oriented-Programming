b9ebfa23fdde7a91845c083d94823fae1390a838
ayanbhatta <ayanb8127@gmail.com>
2025-12-30 13:56:48 +0530

    More problems with different objects

---
+++ oopsMod/src/com/jspider/passingandreturningobjects/Mainclass4.java
@@ -0,0 +1,11 @@
+package com.jspider.passingandreturningobjects;
+
+public class Mainclass4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Player p1 =Dream11.createPlayer();
+		Dream11.displayPlayerDetails(p1);
+	}
+
+}
