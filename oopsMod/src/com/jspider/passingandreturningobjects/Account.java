a33b67ee5c6a30c1c7ab82797e5c734b5d0e2130
ayanbhatta <ayanb8127@gmail.com>
2025-12-31 13:30:27 +0530

    returning objects

---
+++ oopsMod/src/com/jspider/passingandreturningobjects/Account.java
@@ -0,0 +1,10 @@
+package com.jspider.passingandreturningobjects;
+
+public class Account {
+	long accNum;
+	double accBalance;
+	Account(long accNum,double accBalance){
+		this.accNum=accNum;
+		this.accBalance=accBalance;
+	}
+}
