b024eaff6c8a8693e6783b686ede32888649a1c5
ayanbhatta <ayanb8127@gmail.com>
2026-01-14 20:37:53 +0530

    encapsulation

---
+++ oopsMod/src/com/jspider/encapsulation/Mainclass1.java
@@ -0,0 +1,12 @@
+package com.jspider.encapsulation;
+
+public class Mainclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p = new Person();
+		p.setAge(22);
+		System.out.println(p.getAge());
+	}
+
+}
---
+++ oopsMod/src/com/jspider/encapsulation/Person.java
@@ -0,0 +1,12 @@
+package com.jspider.encapsulation;
+
+public class Person {
+	private int age;
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
